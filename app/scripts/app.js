54ed08431d793d0f5affbc7ca9844d2bdd425871
gsgritt <geronimo.garcia@globant.com>
2014-07-21 16:12:14 -0300

    Module example

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,7 @@
 
 'use strict';
 
-angular.module('MyApp', ['ngRoute'])
+angular.module('MyApp', ['ngRoute', 'Sarasa'])
     .config(function($routeProvider){
         $routeProvider
             .when('/', {
@@ -18,13 +18,25 @@ angular.module('MyApp', ['ngRoute'])
             .otherwise({ redirectTo: '/' });
     })
 
-    .controller('HomeCtrl', ['$rootScope', '$scope',
-    function($rootScope, $scope){
+    .controller('HomeCtrl', ['$rootScope', '$scope', 'SarasaFactory',
+    function($rootScope, $scope, SarasaFactory){
         $rootScope.rootName = 'Root var';
         $scope.name = 'Gerónimo';
+        $scope.saySarasa = function(){
+            console.log(SarasaFactory.saySarasa());
+        }
     }])
 
     .controller('HelloCtrl', ['$rootScope', '$scope',
     function($rootScope, $scope){
         $scope.name = 'Superman';
+    }]);
+
+angular.module('Sarasa', [])
+.factory('SarasaFactory', [function(){
+        return {
+            saySarasa: function(){
+                return 'SARASA!!';
+            }
+        }
     }]);
