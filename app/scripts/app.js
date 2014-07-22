86763a155ce4c636040f36336908211ce0a849a2
gsgritt <geronimo.garcia@globant.com>
2014-07-22 15:38:55 -0300

    Clase 2: Scopes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,7 @@
 
 'use strict';
 
-angular.module('MyApp', ['ngRoute', 'Sarasa'])
+angular.module('MyApp', ['ngRoute'])
     .config(function($routeProvider){
         $routeProvider
             .when('/', {
@@ -18,25 +18,61 @@ angular.module('MyApp', ['ngRoute', 'Sarasa'])
             .otherwise({ redirectTo: '/' });
     })
 
-    .controller('HomeCtrl', ['$rootScope', '$scope', 'SarasaFactory',
-    function($rootScope, $scope, SarasaFactory){
-        $rootScope.rootName = 'Root var';
-        $scope.name = 'Gerónimo';
-        $scope.saySarasa = function(){
-            console.log(SarasaFactory.saySarasa());
-        }
+    .controller('HomeCtrl', ['$rootScope', '$scope', 'MyService',
+    function($rootScope, $scope, MyService){
+        $scope.array = [1, 2, 3, 4];
+        $scope.obj = { a: 1, b: 2};
+        $scope.message = "No message";
+
+        var printMsg = function(scope){
+            return function(data){
+                console.log(scope);
+                console.log(data);
+                $scope.message = "Scope is: " + scope + " event: " + data.name;
+            };
+        };
+
+        $rootScope.$on('notifyRootScope', printMsg('$rootScope'));
+        $rootScope.$on('notifyParent', printMsg('$rootScope'));
+        $rootScope.$on('notifyOthers', printMsg('$rootScope'));
+
+        $scope.$on('notifyChildren', printMsg('parent $scope'));
+        $scope.$on('notifyParent', printMsg('parent $scope'));
+
+
+        $scope.notifyChildren = function(){
+            $rootScope.$broadcast('notifyChildren', [ 'value from $rootScope' ]);
+        };
+
+        $scope.emitToRootScope = function(){
+            $scope.$emit('notifyRootScope', [ 'Value from $scope' ]);
+        };
+    }])
+
+    .controller('NestedCtrl', ['$rootScope', '$scope',
+    function($rootScope, $scope){
+        var printMsg = function(scope){
+            return function(data){
+                console.log(scope);
+                console.log(data);
+                $scope.message = "Scope is: " + scope + " event: " + data.name;
+            };
+        };
+
+        $scope.$on('notifyChildren', printMsg('nested $scope'));
+
+        $scope.emitToParentScope = function(){
+            $scope.$emit('notifyParent', [ 'Value from nested $scope' ]);
+        };
     }])
 
     .controller('HelloCtrl', ['$rootScope', '$scope',
     function($rootScope, $scope){
-        $scope.name = 'Superman';
-    }]);
+        $rootScope.$broadcast('notifyOthers', ['notifying others!']);
+    }])
 
-angular.module('Sarasa', [])
-.factory('SarasaFactory', [function(){
+    .factory('MyService', [function(){
         return {
-            saySarasa: function(){
-                return 'SARASA!!';
-            }
-        }
+            doStuff : function(){ return "Doing stuff!! Really busy!!"; }
+        };
     }]);
